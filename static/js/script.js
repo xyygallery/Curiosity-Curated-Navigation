dfef2ce81e94e8597da7a03997ce56799a39a48a
xty6 <86100968+a36as@users.noreply.github.com>
2025-02-15 16:53:07 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -31,19 +31,4 @@ window.onload = updateDateInfo;
 
 
 
-  function updateWebsiteCount() {
-    // 获取所有的 <a> 标签
-    const links = document.querySelectorAll('a');
-    
-    // 获取链接的数量
-    const totalLinks = links.length;
-    
-    // 获取显示总数的元素，并更新其内容
-    const countDisplay = document.getElementById('website-count');
-    countDisplay.textContent = 总收录 ${totalLinks} 个网站;
-}
-
-// 页面加载时自动更新统计
-window.onload = function() {
-    updateWebsiteCount(); // 页面加载时更新统计
-}
+  
